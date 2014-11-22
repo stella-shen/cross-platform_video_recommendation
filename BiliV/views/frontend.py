bdb298dd6528dfe9848f52548cfc661fcfe42357
sz11 <szsxt818@gmail.com>
2014-11-22 19:36:33 +0800

    access_token get test

--- BiliV/views/frontend.py
+++ BiliV/views/frontend.py
@@ -19,14 +19,22 @@ def login():
 		return redirect(url_for('.show'))
 	
 	pOAuth = sina.privateOAuth(APP_KEY, APP_SECRET, CALLBACK_URL)
+	print "test"
 	authorize_url = pOAuth.get_auth_url()
-	return redirect(authorize_url)
-	
-@frontend.route('/callback')
-def callback():
+	path = redirect(authorize_url)
 	code = request.args.get('code', 0)
-	print code
-	return
+	session['code'] = code
+	return path
+	
+@frontend.route('/callback', methods = ['GET', 'POST'])
+def callback():
+	code = session['code']
+	accessOauth = sina.privateOAuth(APP_KEY, APP_SECRET, CALLBACK_URL, code)
+	access_token_url = accessOauth.get_access_token_url()
+	redirect(access_token_url)
+	access_token = request.args.get('access_token', 0)
+	session['access_token'] = access_token
+	return render_template('fronted/show.html')
 
 @frontend.route('/show')
 def show():
--- SNS/sina/privateOAuth.py
+++ SNS/sina/privateOAuth.py
@@ -4,11 +4,15 @@ class privateOAuth(object):
 	auth_host = 'https://api.weibo.com'
 	auth_root = '/oauth2/authorize?'
 
-	def __init__(self, app_key, app_secret, call_back_url):
+	def __init__(self, app_key, app_secret, call_back_url, code):
 		self.app_key = app_key
 		self.app_secret = app_secret
 		self.call_back_url = call_back_url
+		self.code = code
 
 	def get_auth_url(self):
 		auth_url = self.auth_host + self.auth_root + 'client_id=' + self.app_key + '&response_type=code&redirect_uri=' + str(self.call_back_url)
 		return auth_url
+
+	def get_access_token_url(self):
+		access_url = slef.auth_host + '/oauth2/access_token?client_id=' + self.app_key + '&client_secret=' + self.app_secret + '&grant_type=authorization_code&redirect_uri=' + self.call_back_url + '&code=' + self.code
